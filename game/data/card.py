27e74436296b8dadc069e0adf6d7d21b72e10e8c
hogsmill <info@hogsmill.com>
2020-04-17 18:58:21 +0100

    fix .gitignore

---
+++ game/data/card.py
@@ -0,0 +1,16 @@
+
+class Card:
+
+  def prepare_card(self, card):
+    card['assigned'] = []
+    card['remaining'] = card['level']
+    card['completed'] = 0
+
+    return card
+
+  def card_is_complete(self, card):
+    return card['completed'] >= card['level']
+
+  def complete_work_on_card(self, card, amount):
+    card['completed'] = card['completed'] + amount
+    card['remaining'] = card['remaining'] - amount
